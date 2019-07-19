2f7364a71d1524e3f94f0006ff3ee4e95a6e0a70
Tom Bawel <tbawel@gmail.com>
2019-07-18 20:55:48 -0600

    rendered Map and OtherMap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home/Home'
+import OtherMap from './components/Map/OtherMap'
 
 class App extends React.Component {
-  render(){
-  return (
-   <Home />
-  );
-}
+  render() {
+    return (
+      <div>
+        <Home />
+        <OtherMap />
+      </div>
+    );
+  }
 }
 export default App;
