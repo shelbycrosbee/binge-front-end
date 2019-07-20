3149209372b85bc999f9abf963e36aa524e01583
Shelby Miller <shelbycrosbee@gmail.com>
2019-07-20 09:14:51 -0600

    semanic ui

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './components/Home/Home'
 import OtherMap from './components/Map/OtherMap'
 
+
 class App extends React.Component {
   render() {
     return (
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Button from './Button'
+import Header from './Header'
+import 'semantic-ui-css/semantic.min.css'
 
-
-class Home extends React.Component{
-  render(){
-  return (
-    <div>
-   <h1>Are You Done Binging?!</h1>
-   <Button text='yes, help!'/>{` `}
-   <Button text={`hell no \\m/ :P`}/>
-   </div>
-  );
-}
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>Are You Done Binging?!</h1>
+        <Button text='yes, help!' />{` `}
+        <Button text={`hell no \\m/ :P`} />
+      </div>
+    );
+  }
 }
 export default Home;
