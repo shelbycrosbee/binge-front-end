bf126bbdd75436e31a3781f02a8e9189e9a468ee
Shelby Miller <shelbycrosbee@gmail.com>
2019-07-20 16:19:09 -0600

    trying to call api

--- src/components/Map/HospitalsMap.js
+++ src/components/Map/HospitalsMap.js
@@ -4,8 +4,40 @@ import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
 
 
 class HospitalsMap extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      hospitals: [
 
-  
+      ] 
+    }
+  }
+
+  componentDidMount() {
+    axios.get("https://places.cit.api.here.com/places/v1/discover/explore", {
+      params: {
+        app_id: 'm7oaMKyzvBVSCbZkqVNh',
+        app_code: "wz6uMQMRHOibXv9XZSR6lg",
+        at: '45.6796,-111.0363', 
+        cat: 'hospital-health-care-facility'
+
+      }
+    })
+      .then(response => {
+        console.log(results.items.position);
+        let hospitals = response.items.position.map((hospital, i) => {
+          return {
+            id: i,
+            title: `${hospital.title} ${hospital.position}`,
+          }
+        })
+        this.setState({ hospitals })
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
+
   render() {
     return (
       <LeafletMap
