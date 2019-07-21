b928381fe1a2ce855ad4b2d8f3a416ac6d2fc4f6
Tom Bawel <tbawel@gmail.com>
2019-07-21 10:51:13 -0600

    user drop pin

--- src/components/Home/No.js
+++ src/components/Home/No.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import OtherMap from '../Map/OtherMap';
-import Header from '../Home/Header'
+import Header from '../Home/Header';
 
 class No extends React.Component {
 
-render(){
-  return(
-    <div>
-    <Header/>
-    <OtherMap />
+  render() {
+    return (
+     <div>
+      <Header />
+      <OtherMap /> 
     </div>
-  );
-}
-
+    )
+  }
 }
 
 export default No
--- src/components/Map/OtherMap.js
+++ src/components/Map/OtherMap.js
@@ -2,9 +2,24 @@ import React from 'react'
 import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
 
 class OtherMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPos: null
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+
+  handleClick(e) {
+    this.setState({ currentPos: e.latlng });
+  }
+
+
   render() {
     return (
       <LeafletMap
+        onClick={this.handleClick}
         center={[45.7770, -110.9429]}
         zoom={6}
         maxZoom={20}
@@ -19,11 +34,11 @@ class OtherMap extends React.Component {
         <TileLayer
           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
         />
-        <Marker position={[45.6796, -111.0363]}>
-          <Popup>
-            Popup for any custom information.
+        {this.state.currentPos && <Marker position={this.state.currentPos} draggable={false}>
+          <Popup position={this.state.currentPos}>
+            Current location: <pre>{JSON.stringify(this.state.currentPos, null, 2)}</pre>
           </Popup>
-        </Marker>
+        </Marker>}
       </LeafletMap>
     );
   }
